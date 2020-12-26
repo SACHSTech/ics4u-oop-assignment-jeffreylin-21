3aa81f40e47485afff59c7c91701f2931f7a3f16
Jeffrey Lin <jeffrey.lin21@ycdsbk12.ca>
2020-12-26 19:05:02 +0000

    added getter/setter methods for plane

--- src/Battleship/Plane.java
+++ src/Battleship/Plane.java
@@ -4,16 +4,23 @@ public class Plane {
 	
 	private String strPlaneName;
 	private int intBombSpread;
-  private boolean isAlive;
 	private boolean isScout;
 	
 	public Plane (String strPlaneName, int intBombSpread, boolean isScout) {
 		this.strPlaneName = strPlaneName;
 		this.intBombSpread = intBombSpread;
     this.isScout = isScout;
-		this.isAlive = true;
 	}
 
-  
+  public int getSpread(){
+    return this.intBombSpread;
+  }
 	
+  public boolean getScout(){
+    return this.isScout;
+  }
+
+  public String getName(){
+    return this.strPlaneName;
+  }
 }
