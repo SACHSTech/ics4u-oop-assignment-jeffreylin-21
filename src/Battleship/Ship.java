25a56cd012cf7b9f07ee811e5fdbd3471b284dce
Jeffrey Lin <jeffrey.lin21@ycdsbk12.ca>
2020-12-26 18:31:20 +0000

    Added getter/setter methods for ship

--- src/Battleship/Ship.java
+++ src/Battleship/Ship.java
@@ -3,17 +3,40 @@ package Battleship;
 public class Ship {
 	
 	private String strType;
-	private int intShipsSunk;
+	private int intShipSunk;
 	private int intHealth;
 	private boolean isDestroyed;	
 	
-	public Ship (String strType, int intHealth) {
+	public Ship (String strType, int intHealth, boolean isDestroyed) {
 
 		this.strType = strType;
 		this.intHealth = intHealth;
     this.intShipsSunk = 0;
-		this.isDestroyed = false;
-    
+		this.isDestroyed = isDestroyed;
+
 	}
-		
+
+  public String getType(){
+    return this.getType;
+  }
+
+  public int getHealth() {
+    return this.intHealth;
+  }
+
+  public void setHealth() {
+    this.intHealth--;
+    if (intHealth == 0) {
+      this.changeState();
+    }
+  } 
+
+  public void changeState() {
+    this.isDestroyed = true;
+  }
+
+  public boolean getState(){
+    return this.isDestroyed;
+  }
+
 }
